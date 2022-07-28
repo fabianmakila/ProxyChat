c4b8dee0fda0f64f997dd29129a97c60789e6ade
fabianmakila <contact@fabianadrian.fi>
2022-07-28 16:02:15 +0300

    Use optional BooleanArgument for spy and toggle

--- plugin/src/main/java/fi/fabianadrian/proxychat/command/commands/MessagesCommand.java
+++ plugin/src/main/java/fi/fabianadrian/proxychat/command/commands/MessagesCommand.java
@@ -1,5 +1,6 @@
 package fi.fabianadrian.proxychat.command.commands;
 
+import cloud.commandframework.arguments.standard.BooleanArgument;
 import cloud.commandframework.context.CommandContext;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.proxy.Player;
@@ -9,6 +10,8 @@ import fi.fabianadrian.proxychat.command.CommandPermissions;
 import fi.fabianadrian.proxychat.locale.Messages;
 import fi.fabianadrian.proxychat.user.User;
 
+import java.util.Optional;
+
 public class MessagesCommand extends AbstractCommand {
 
     public MessagesCommand(ProxyChat proxyChat) {
@@ -17,24 +20,26 @@ public class MessagesCommand extends AbstractCommand {
 
     @Override
     public void register() {
-        var builder = this.commandManager.commandBuilder("messages", "msgs");
+        var builder = this.commandManager.commandBuilder("messages", "msgs").senderType(Player.class);
 
-        this.commandManager.command(builder.literal("spy").permission(CommandPermissions.MESSAGES_SPY).senderType(Player.class).handler(this::executeSpy));
-        this.commandManager.command(builder.literal("toggle").permission(CommandPermissions.MESSAGES_TOGGLE).senderType(Player.class).handler(this::executeToggle));
+        this.commandManager.command(builder.literal("spy").permission(CommandPermissions.MESSAGES_SPY).argument(BooleanArgument.optional("enabled")).handler(this::executeSpy));
+        this.commandManager.command(builder.literal("toggle").permission(CommandPermissions.MESSAGES_TOGGLE).argument(BooleanArgument.optional("enabled")).handler(this::executeToggle));
     }
 
     private void executeSpy(CommandContext<CommandSource> ctx) {
+        Optional<Boolean> enabledOptional = ctx.getOptional("enabled");
         User user = this.proxyChat.userManager().user((Player) ctx.getSender());
+        boolean value = enabledOptional.orElseGet(() -> !user.spying());
 
-        boolean value = !user.spying();
         user.spying(value);
         ctx.getSender().sendMessage(value ? Messages.COMMAND_MESSAGES_SPY_ENABLE : Messages.COMMAND_MESSAGES_SPY_DISABLE);
     }
 
     private void executeToggle(CommandContext<CommandSource> ctx) {
+        Optional<Boolean> enabledOptional = ctx.getOptional("enabled");
         User user = this.proxyChat.userManager().user((Player) ctx.getSender());
+        boolean value = enabledOptional.orElseGet(() -> !user.messages());
 
-        boolean value = !user.messages();
         user.messages(value);
         ctx.getSender().sendMessage(value ? Messages.COMMAND_MESSAGES_TOGGLE_ENABLE : Messages.COMMAND_MESSAGES_TOGGLE_DISABLE);
     }
