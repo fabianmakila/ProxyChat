e91dfb1c9daca7e64e3fbab7b3a25f1cdab6cb31
fabianmakila <contact@fabianadrian.fi>
2023-04-30 19:23:30 +0300

    fix: use empty list as a default value

--- common/src/main/java/fi/fabianadrian/proxychat/common/config/AnnouncementsConfig.java
+++ common/src/main/java/fi/fabianadrian/proxychat/common/config/AnnouncementsConfig.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 public interface AnnouncementsConfig {
 
-    @ConfDefault.DefaultStrings("")
+    static List<String> emptyList() {
+        return List.of();
+    }
+
+    @ConfDefault.DefaultObject("emptyList")
     List<String> announcements();
 
     @ConfDefault.DefaultInteger(30)
