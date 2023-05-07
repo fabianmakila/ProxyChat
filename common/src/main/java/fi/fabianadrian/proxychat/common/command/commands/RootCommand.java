919a561341fcd42e9e9e99f99084412495f2ff14
fabianmakila <contact@fabianadrian.fi>
2023-05-07 17:26:36 +0300

    chore: cleanup

--- common/src/main/java/fi/fabianadrian/proxychat/common/command/commands/RootCommand.java
+++ common/src/main/java/fi/fabianadrian/proxychat/common/command/commands/RootCommand.java
@@ -3,9 +3,10 @@ package fi.fabianadrian.proxychat.common.command.commands;
 import cloud.commandframework.context.CommandContext;
 import fi.fabianadrian.proxychat.common.ProxyChat;
 import fi.fabianadrian.proxychat.common.command.Commander;
+import fi.fabianadrian.proxychat.common.command.ProxyChatCommand;
 import fi.fabianadrian.proxychat.common.locale.Messages;
 
-public final class RootCommand extends fi.fabianadrian.proxychat.common.command.ProxyChatCommand {
+public final class RootCommand extends ProxyChatCommand {
     public RootCommand(ProxyChat proxyChat) {
         super(proxyChat, "proxychat");
     }
