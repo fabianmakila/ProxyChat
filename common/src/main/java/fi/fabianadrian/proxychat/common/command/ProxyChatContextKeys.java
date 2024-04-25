f5fc30ab8cc9d1599bfc70459c8c278f1e573340
FabianAdrian <contact@fabianadrian.fi>
2024-04-25 22:46:28 +0300

    refactor: simplify ProxyChatContextKeys

--- common/src/main/java/fi/fabianadrian/proxychat/common/command/ProxyChatContextKeys.java
+++ common/src/main/java/fi/fabianadrian/proxychat/common/command/ProxyChatContextKeys.java
@@ -3,23 +3,12 @@ package fi.fabianadrian.proxychat.common.command;
 import fi.fabianadrian.proxychat.common.channel.ChannelRegistry;
 import fi.fabianadrian.proxychat.common.hook.HookManager;
 import fi.fabianadrian.proxychat.common.user.UserManager;
-import io.leangen.geantyref.TypeToken;
 import org.incendo.cloud.key.CloudKey;
 
 public final class ProxyChatContextKeys {
-
-	public static final CloudKey<ChannelRegistry> CHANNEL_REGISTRY_KEY = CloudKey.of(
-			"ChannelRegistry",
-			TypeToken.get(ChannelRegistry.class)
-	);
-	public static final CloudKey<HookManager> HOOK_MANAGER_KEY = CloudKey.of(
-			"HookManager",
-			TypeToken.get(HookManager.class)
-	);
-	public static final CloudKey<UserManager> USER_MANAGER_KEY = CloudKey.of(
-			"UserManager",
-			TypeToken.get(UserManager.class)
-	);
+	public static final CloudKey<ChannelRegistry> CHANNEL_REGISTRY_KEY = CloudKey.of("ChannelRegistry", ChannelRegistry.class);
+	public static final CloudKey<HookManager> HOOK_MANAGER_KEY = CloudKey.of("HookManager", HookManager.class);
+	public static final CloudKey<UserManager> USER_MANAGER_KEY = CloudKey.of("UserManager", UserManager.class);
 
 	private ProxyChatContextKeys() {
 	}
